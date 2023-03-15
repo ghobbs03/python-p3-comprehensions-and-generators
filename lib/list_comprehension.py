620779dc737353da687b305c4929139ec9459ec6
Gail Hobbs <gbhobbs97@gmail.com>
2023-03-14 21:25:56 -0700

    completed

--- lib/list_comprehension.py
+++ lib/list_comprehension.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 
 def return_evens(num_list):
-    pass
+    return [num_list[i] for i in range(len(num_list)) if num_list[i] % 2 == 0 ]
 
 def make_exclamation(sentence_list):
-    pass
+    return [sentence + "!" for sentence in sentence_list]
